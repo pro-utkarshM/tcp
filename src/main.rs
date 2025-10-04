c993b998c6eacc0338f113bee3620a1493fda7f9
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2025-10-04 23:50:03 +0530

    parsing packet info

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn main() -> io::Result<()> {
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
         let read_bytes = &buf[..nbytes];
+        
+        //eth_frame from the link level
         let flags = u16::from_be_bytes([read_bytes[0], read_bytes[1]]);
         let proto = u16::from_be_bytes([read_bytes[2], read_bytes[3]]);
 
@@ -17,8 +19,14 @@ fn main() -> io::Result<()> {
         }
         match etherparse::Ipv4HeaderSlice::from_slice(&read_bytes[4..nbytes]) {
             Ok(packet) => {
-                eprintln!("Flags: {:#06x}, Proto: {:#06x}", flags, proto);
-                eprintln!("Read {} bytes: {:?}", nbytes - 4, packet);
+                eprintln!(
+                    "IPv4 packet: {} -> {}, {} bytes of protocol: {:?}, flags: {:#06x}",
+                    packet.source_addr(),
+                    packet.destination_addr(),
+                    packet.payload_len().unwrap_or(0),
+                    packet.protocol(),
+                    flags
+                );
             },
             Err(e) => {
                 eprintln!("Failed to parse IPv4 header: {}", e);
