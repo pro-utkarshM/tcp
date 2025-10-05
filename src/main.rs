6e83bad78e5ce3c90d0ec370372cdb8e0162e388
Utkarsh Maurya <projects.utkarshMaurya@gmail.com>
2025-10-05 18:37:59 +0530

    tcp header parsing

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,35 @@ fn main() -> io::Result<()> {
         }
         match etherparse::Ipv4HeaderSlice::from_slice(&read_bytes[4..nbytes]) {
             Ok(packet) => {
+                let proto = packet.protocol();  
+                if proto != etherparse::IpNumber::from(0x06) {
+                    eprint!("Non TCP packet received, proto={:?}", proto);
+                    continue;
+                }
+                match etherparse::TcpHeaderSlice::from_slice(&read_bytes[4 + packet.slice().len()..nbytes]) {
+                    Ok(tcp_header) => {
+                        eprintln!(
+                            "TCP packet: {}:{} -> {}:{}, seq={}, ack={}",
+                            packet.source_addr(),
+                            tcp_header.source_port(),
+                            packet.destination_addr(),
+                            tcp_header.destination_port(),
+                            tcp_header.sequence_number(),
+                            tcp_header.acknowledgment_number(),
+                        );
+                },
+                    Err(e) => {
+                        eprintln!("Failed to parse TCP header: {}", e);
+                        continue;
+                    }
+                }
+                // Print out some information about the packet
+                // including the flags from the link layer header
+                // (for TUN, flags are usually 0x0000 or 0x0004)
+                // For TAP, flags may include 0x0001 (broadcast) or
+                // 0x0002 (multicast)
+                // See https://www.kernel.org/doc/html/latest/networking/packet_mmap.html
+                // for more details about the flags
                 eprintln!(
                     "IPv4 packet: {} -> {}, {} bytes of protocol: {:?}, flags: {:#06x}",
                     packet.source_addr(),
